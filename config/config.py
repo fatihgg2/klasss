0309866fe74512f36f94745c47c5b47470574142
GoktugResmi <143345012+GoktugResmi@users.noreply.github.com>
2023-09-26 00:06:10 +0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -224,7 +224,7 @@ PING_IMG_URL = getenv(
 
     "PING_IMG_URL",
 
-    "assets/Ping.jpeg",
+    "https://graph.org/file/511bc4c2d50a0e97a8734.jpg",
 
 )
 
@@ -232,7 +232,7 @@ PLAYLIST_IMG_URL = getenv(
 
     "PLAYLIST_IMG_URL",
 
-    "assets/Playlist.jpeg",
+    "https://graph.org/file/e0f5fe7ff6172bd632af0.jpg",
 
 )
 
@@ -240,7 +240,7 @@ GLOBAL_IMG_URL = getenv(
 
     "GLOBAL_IMG_URL",
 
-    "assets/Global.jpeg",
+    "https://graph.org/file/e0f5fe7ff6172bd632af0.jpg",
 
 )
 
@@ -248,7 +248,7 @@ STATS_IMG_URL = getenv(
 
     "STATS_IMG_URL",
 
-    "assets/Stats.jpeg",
+    "https://graph.org/file/46caae8b1308485b049c4.jpg",
 
 )
 
@@ -256,7 +256,7 @@ TELEGRAM_AUDIO_URL = getenv(
 
     "TELEGRAM_AUDIO_URL",
 
-    "assets/Audio.jpeg",
+    "https://graph.org/file/e0f5fe7ff6172bd632af0.jpg",
 
 )
 
@@ -264,7 +264,7 @@ TELEGRAM_VIDEO_URL = getenv(
 
     "TELEGRAM_VIDEO_URL",
 
-    "assets/Video.jpeg",
+    "https://graph.org/file/e0f5fe7ff6172bd632af0.jpg",
 
 )
 
@@ -272,7 +272,7 @@ STREAM_IMG_URL = getenv(
 
     "STREAM_IMG_URL",
 
-    "assets/Stream.jpeg",
+    "https://graph.org/file/e0f5fe7ff6172bd632af0.jpg",
 
 )
 
@@ -280,7 +280,7 @@ SOUNCLOUD_IMG_URL = getenv(
 
     "SOUNCLOUD_IMG_URL",
 
-    "assets/Soundcloud.jpeg",
+    "https://graph.org/file/e0f5fe7ff6172bd632af0.jpg",
 
 )
 
@@ -288,7 +288,7 @@ YOUTUBE_IMG_URL = getenv(
 
     "YOUTUBE_IMG_URL",
 
-    "assets/Youtube.jpeg",
+    "https://graph.org/file/e0f5fe7ff6172bd632af0.jpg",
 
 )
 
@@ -296,7 +296,7 @@ SPOTIFY_ARTIST_IMG_URL = getenv(
 
     "SPOTIFY_ARTIST_IMG_URL",
 
-    "assets/SpotifyArtist.jpeg",
+    "https://graph.org/file/e0f5fe7ff6172bd632af0.jpg",
 
 )
 
@@ -304,7 +304,7 @@ SPOTIFY_ALBUM_IMG_URL = getenv(
 
     "SPOTIFY_ALBUM_IMG_URL",
 
-    "assets/SpotifyAlbum.jpeg",
+    "https://graph.org/file/e0f5fe7ff6172bd632af0.jpg",
 
 )
 
@@ -312,7 +312,7 @@ SPOTIFY_PLAYLIST_IMG_URL = getenv(
 
     "SPOTIFY_PLAYLIST_IMG_URL",
 
-    "assets/SpotifyPlaylist.jpeg",
+    "https://graph.org/file/e0f5fe7ff6172bd632af0.jpg",
 
 )
 
