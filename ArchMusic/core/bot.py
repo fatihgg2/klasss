423dd9b6e80e06e4395556ec477bf027e95714c7
GoktugResmi <143345012+GoktugResmi@users.noreply.github.com>
2023-09-25 10:58:58 +0300

    Update bot.py

--- ArchMusic/core/bot.py
+++ ArchMusic/core/bot.py
@@ -10,7 +10,7 @@ from ..logging import LOGGER
 
 class ArchMusic(Client):
     def __init__(self):
-        LOGGER(__name__).info(f"Starting Bot")
+        LOGGER(__name__).info(f"Bot Başladı . . . 🎉")
         super().__init__(
             "ArchMusic",
             api_id=config.API_ID,
